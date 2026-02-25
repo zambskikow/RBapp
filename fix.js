63ed31ab690146f3f3e4453d876b6660e5eba9b7
zambskikow <patriciobrito88@gmail.com>
2026-02-24 23:42:11 -0300

    Delete fix.js

--- fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = 'js/app.js';
-let c = fs.readFileSync(path, 'utf8');
-c = c.replace(/class="badge \${log\.permissao ===/g, 'class="table-badge ${log.permissao ===');
-fs.writeFileSync(path, c, 'utf8');
